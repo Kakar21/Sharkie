f19b6f11636fd870f7d5c5ef5249bbb4b78c73f0
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-01-22 23:26:09 +0100

    animated character

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,10 +4,19 @@ class MoveableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image">
         this.img.src = path;
+    }
+
+    loadImages(array) {
+        array.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
